e61611b55cd8f39a02d382f1e633886de1aaa4de
vineeta kshatriya <vineeta.kshatriya@viit.ac.in>
2021-05-11 23:31:23 +0530

    changed X axis Title

--- graphs.py
+++ graphs.py
@@ -9,7 +9,7 @@ def bar_graph(data, go, st):
     Bars = list(go.Bar(name=name, x=df["time"], y=df[name]) for name in columns)
 
     fig = go.Figure(data=Bars, layout=layout)
-    fig.update_xaxes(title_text="Index")
+    fig.update_xaxes(title_text="Timestamp")
     fig.update_yaxes(title_text="Values", zeroline=True)
     fig.update_layout(barmode='group')
     st.plotly_chart(fig)
